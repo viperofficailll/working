03559abc353230cee260b209e3d6fb2ffe1b8903
0123trying <be2021ce138@gces.edu.np>
2025-05-28 12:06:32 +0545

    frontend

---
+++ frontend/src/App.tsx
@@ -0,0 +1,12 @@
+import { Button } from "./components/Button"; 
+
+function App() {
+  return (
+    <>
+      <Button varient="primary" text="Share" onClick={() => {}} size="sm" />
+        <Button varient="secondary" text="Add content" onClick={()=>{}} size="lg"></Button>
+    </>
+  );
+}
+
+export default App;
